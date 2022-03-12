cfceadbf0f5b6b56f09be3eb06ee2dd071bca384
Ankit Goswami <ankitgoswami@Ankits-MacBook-Pro.local>
2022-03-12 18:29:43 -0500

    Remove test schedules

--- config/initializers/pi_schedule.rb
+++ config/initializers/pi_schedule.rb
@@ -2,16 +2,6 @@ require 'rufus-scheduler'
 
 scheduler = Rufus::Scheduler.new
 
-scheduler.in '60s' do
-    on_command = "vcgencmd display_power 0"
-    system(on_command)
-end
-
-scheduler.in '90s' do
-    on_command = "vcgencmd display_power 1"
-    system(on_command)
-end
-
 scheduler.cron '30 08 * * *' do
     on_command = "vcgencmd display_power 1"
     system(on_command)
