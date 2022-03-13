a08894b6382fad5026cdb50cfe14fc987efb4740
Ankit Goswami <ankitgoswami@Ankits-MacBook-Pro.local>
2022-03-12 22:26:14 -0500

    Change shutdown time to 11

--- config/initializers/pi_schedule.rb
+++ config/initializers/pi_schedule.rb
@@ -7,7 +7,7 @@ scheduler.cron '30 08 * * *' do
     system(on_command)
 end
 
-scheduler.cron '00 22 * * *' do
+scheduler.cron '00 23 * * *' do
     off_command = "vcgencmd display_power 0"
     system(off_command)
 end
