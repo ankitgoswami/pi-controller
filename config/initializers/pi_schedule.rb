0692572fce78ac4930deafad52e4a5a2fb8ebfae
Ankit Goswami <ankitgoswami@Ankits-MacBook-Pro.local>
2022-03-13 11:20:01 -0400

    update schedules

--- config/initializers/pi_schedule.rb
+++ config/initializers/pi_schedule.rb
@@ -2,13 +2,17 @@ require 'rufus-scheduler'
 
 scheduler = Rufus::Scheduler.new
 
-scheduler.cron '30 08 * * *' do
+scheduler.cron '30 12 * * *' do
     on_command = "vcgencmd display_power 1"
     system(on_command)
 end
 
-scheduler.cron '00 23 * * *' do
+scheduler.cron '00 03 * * *' do
     off_command = "vcgencmd display_power 0"
     system(off_command)
 end
 
+scheduler.cron '25 14 * * *' do
+    off_command = "vcgencmd display_power 0"
+    system(off_command)
+end
